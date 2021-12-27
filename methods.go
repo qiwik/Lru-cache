096a5f1f0efbcba111067f2bf70036f604bb9bf4
Kirill Kuzin <qiwikuz@yandex.ru>
2021-12-27 23:45:13 +0300

    added comments

--- methods.go
+++ methods.go
@@ -5,76 +5,9 @@ import (
 	"reflect"
 )
 
-func (c *Cache) Len() int {
-	return c.chain.Len()
-}
-
-func (c *Cache) Clear() {
-	for c.chain.Len() > 0 {
-		c.removeLast()
-	}
-}
-
-func (c *Cache) ChangeCapacity(newCap int) {
-	switch {
-	case newCap <= 0:
-		return
-	case newCap >= c.capacity:
-		c.capacity = newCap
-		return
-	default:
-		c.capacity = newCap
-		for c.Len() > newCap {
-			c.removeLast()
-		}
-	}
-}
-
-func (c *Cache) ChangeValue(key string, newValue interface{}) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	element, ok := c.validate(key)
-	if !ok {
-		return false
-	}
-
-	element.Value.(*item).value = newValue
-	c.chain.MoveToFront(element)
-	return true
-}
-
-func (c *Cache) Keys() []string {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	keys := make([]string, 0, len(c.items))
-	for key := range c.items {
-		keys = append(keys, key)
-	}
-	return keys
-}
-
-func (c *Cache) ReflectKeys() []string {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	keysValues := reflect.ValueOf(c.items).MapKeys()
-	keys := make([]string, 0, len(c.items))
-	for i := range keysValues {
-		keys = append(keys, keysValues[i].String())
-	}
-	return keys
-}
-
-func (c *Cache) Values() []interface{} {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	values := make([]interface{}, 0, len(c.items))
-	for _, value := range c.items {
-		values = append(values, value)
-	}
-	return values
-}
-
-// Add returns false if current key already exists, and true if key doesn't exist and new item was added to cache
+// Add returns false if current key already exists, and true if key doesn't exist and new item was added to cache.
+// When a new element is added, it is placed at the top of the list, and if capacity is reached, the last element,
+// which is also the most unpopular in the cache, is deleted
 func (c *Cache) Add(key string, value interface{}) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -95,13 +28,22 @@ func (c *Cache) Add(key string, value interface{}) bool {
 	return true
 }
 
-func (c *Cache) removeLast() {
-	currentElement := c.chain.Back()
-	last := c.chain.Remove(currentElement).(*item)
-	delete(c.items, last.key)
+// Get func returns a value with true if such element exist with current key, else returns nil and false. If an element
+// exists, it is moved to the top of the list in the cache
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	element, ok := c.validate(key)
+	if !ok {
+		return nil, false
+	}
+
+	value := element.Value.(*item).value
+	c.chain.MoveToFront(element)
+	return value, true
 }
 
-// Remove returns false if current key doesn't exist, and true if removing was successful
+// Remove returns false if current key doesn't exist, and true if removing from cache was successful
 func (c *Cache) Remove(key string) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -115,6 +57,22 @@ func (c *Cache) Remove(key string) bool {
 	return true
 }
 
+// ChangeValue allows you to change the value of a key that already exists in the cache. If there is no such key in
+// the cache, the function returns false. If the value has changed, the element is sent to the top of the cache list
+func (c *Cache) ChangeValue(key string, newValue interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	element, ok := c.validate(key)
+	if !ok {
+		return false
+	}
+
+	element.Value.(*item).value = newValue
+	c.chain.MoveToFront(element)
+	return true
+}
+
+// validate checks the existence of an element by the key, and if it does not exist, returns false, instead of an element
 func (c *Cache) validate(key string) (element *list.Element, ok bool) {
 	if element, ok = c.items[key]; !ok {
 		return nil, false
@@ -122,16 +80,75 @@ func (c *Cache) validate(key string) (element *list.Element, ok bool) {
 	return element, true
 }
 
-// Get func returns a value with true if such element exist with current key, else returns nil and false
-func (c *Cache) Get(key string) (interface{}, bool) {
+// Clear completely clears the cache
+func (c *Cache) Clear() {
+	for c.chain.Len() > 0 {
+		c.removeLast()
+	}
+}
+
+// Len allows you to find out the fullness of the cache
+func (c *Cache) Len() int {
+	return c.chain.Len()
+}
+
+// ChangeCapacity allows you to dynamically change the cache capacity. The new value must not be less than one. If
+// the new capacity is less than the previous one, then the last elements in the list are deleted up to the desired
+// parameter value
+func (c *Cache) ChangeCapacity(newCap int) {
+	switch {
+	case newCap <= 0:
+		return
+	case newCap >= c.capacity:
+		c.capacity = newCap
+		return
+	default:
+		c.capacity = newCap
+		for c.Len() > newCap {
+			c.removeLast()
+		}
+	}
+}
+
+// removeLast deletes the last element in the list
+func (c *Cache) removeLast() {
+	currentElement := c.chain.Back()
+	last := c.chain.Remove(currentElement).(*item)
+	delete(c.items, last.key)
+}
+
+// Keys returns a slice of the keys that exist in the cache by simply traversing all the keys. Works faster than
+// a function with reflection
+func (c *Cache) Keys() []string {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	element, ok := c.validate(key)
-	if !ok {
-		return nil, false
+	keys := make([]string, 0, len(c.items))
+	for key := range c.items {
+		keys = append(keys, key)
 	}
+	return keys
+}
 
-	value := element.Value.(*item).value
-	c.chain.MoveToFront(element)
-	return value, true
+// ReflectKeys returns a slice of keys existing in the cache using reflection. It works 3-4 times slower than the Keys
+// function, but is left for variability
+func (c *Cache) ReflectKeys() []string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	keysValues := reflect.ValueOf(c.items).MapKeys()
+	keys := make([]string, 0, len(c.items))
+	for i := range keysValues {
+		keys = append(keys, keysValues[i].String())
+	}
+	return keys
+}
+
+// Values returns a slice of all existing element values in the cache
+func (c *Cache) Values() []interface{} {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	values := make([]interface{}, 0, len(c.items))
+	for _, value := range c.items {
+		values = append(values, value)
+	}
+	return values
 }
--- models.go
+++ models.go
@@ -6,6 +6,13 @@ import (
 	"sync"
 )
 
+// Cache is a structure describing a temporary data store by key through a linked list. Mutex is present inside to
+// implement the ability to work with the cache competitively.
+// The number of entries is fixed. This implementation, rather than a memory limit, is chosen to work directly with
+// storage objects. In addition to the list, there is a hash table with data inside the structure, which is used
+// directly for accounting records.
+// All fields are non-exportable, which allows you to work with the content through methods without having
+// direct access to the cache
 type Cache struct {
 	mu sync.Mutex
 
@@ -14,7 +21,8 @@ type Cache struct {
 	chain    *list.List
 }
 
-// NewCache create new implementation of lru Cache
+// NewCache create new implementation of lru Cache. Capacity can't be less than one. If you set capacity to zero,
+// for example, an assignment error will return
 func NewCache(n int) (*Cache, error) {
 	if n <= 0 {
 		return nil, errors.New("capacity of the cache can not be less than 1")
@@ -26,7 +34,7 @@ func NewCache(n int) (*Cache, error) {
 	}, nil
 }
 
-// item is an element inside *list.Element of Cache
+// item is an element inside *list.Element of Cache with the key and value used by the your program
 type item struct {
 	key   string
 	value interface{}
